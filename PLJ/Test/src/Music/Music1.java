15a83006275c72ee69c9d7a0119002178feec7ad
GyeongHo <kungho84@gmail.com>
2024-02-21 20:27:37 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ PLJ/Test/src/Music/Music1.java
@@ -0,0 +1,33 @@
+package Music;
+
+public class Music1 {
+    public static void main(String[] args) {    //MusicPlayerData 클래스 사용
+        MusicPlayerData data = new MusicPlayerData();   //
+//     new 키워드는 클래스의 생성자를 호출하여 객체를 생성합니다. 생성자는 객체를 초기화하는 역할을 합니다.
+//        생성된 객채는 . 을 통해 접근가능
+
+
+//        음악 플레이어를 시작합니다
+        data.isOn = true;
+        System.out.println("음악 플레이어를 시작합니다.");
+//        음악 플레이어 볼륨:1
+        data.volume++;
+        System.out.println("볼륨을 증가 합니다" + data.volume);
+//        음악 플레이어 볼륨:2
+        data.volume++;
+        System.out.println("볼륨을 증가 합니다" + data.volume);
+//        음악 플레이어 볼륨:1
+        data.volume--;
+        System.out.println("볼륨을 감소 합니다" + data.volume);
+//        음악 플레이어 상태 확인
+        System.out.println("음악 플레이어를 확인합니다");
+        if(data.isOn) {
+            System.out.println("음악 플레이어on, 볼륨: " + data.volume);
+//            음악 플레이어 ON, 볼륨:1
+        } else {
+            System.out.println("프로그램을 종료합니다");
+        }
+//        음악 플레이어를 종료합니다
+    }
+}
+
---
+++ PLJ/Test/src/Music/MusicPlayerData.java
@@ -0,0 +1,15 @@
+/*
+앞서 작성한 코드에 클래스를 도입하자.
+MusicPlayerData 라는 클래스를 만들고, 음악 플레이어에 사용되는 데이터
+들을 여기에 묶어서 멤버 변수로 사용하자.
+ */
+package Music;
+
+public class MusicPlayerData {
+//    볼륨조절 , 음악플레이어 전원 클래스 생성
+    int volume = 0;
+    boolean isOn = false;
+}
+/*클래스는 객체를 만들기 위한 설계도
+클래스에는 객체의 구조 (멤버 변수)와 행동 (멤버 메서드)을 정의
+ */
