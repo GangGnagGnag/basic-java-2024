65b33353d3c4a185ca50fa6bc3d26e4fc6812632
GyeongHo <kungho84@gmail.com>
2024-03-03 23:38:00 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ TestInfrun/src/extends1/super1/SuperMain.java
@@ -0,0 +1,10 @@
+package extends1.super1;
+
+import extends1.access.child.Child;
+
+public class SuperMain {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}
