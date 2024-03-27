b7848f17867e77244cef14ee1bf39764af08eb55
GyeongHo <kungho84@gmail.com>
2024-03-27 11:27:15 +0900

    조금만 쉴래...

---
+++ Any/alone/src/은행관리3/Account.java
@@ -0,0 +1,41 @@
+package 은행관리3;
+
+public class Account {
+    private String accountNum;
+    private String name;
+    private int balance;
+
+    public Account(String accountNum, String name, int balance) {
+        this.accountNum = accountNum;
+        this.name = name;
+        this.balance = balance;
+    }
+    
+//    잔고 조회
+    public int getBalance(){
+        return balance;
+    }
+    
+//    입급하기
+    public void deposit(int deposit){
+        if(deposit <= 0){
+            System.out.println("금액을 확인하세요");
+        }
+        else {
+            balance += deposit;
+        }
+    }
+    
+    public void withdraw(int withdraw){
+        if(withdraw > balance){
+            System.out.println("잔약이 부족합니다");
+        }
+        else {
+            balance -= withdraw;
+        }
+    }
+
+    public String getAccountNum(){
+        return accountNum;
+    }
+}
---
+++ Any/alone/src/은행관리3/Main.java
@@ -0,0 +1,60 @@
+package 은행관리3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    private static ArrayList<Account> accounts = new ArrayList<>();
+    public static Scanner sc = new Scanner(System.in); // 사용가능하게 만들어줌
+
+//    계좌확인 메서드
+    public static int makeAccouontNum(String accountNum) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getAccountNum().equals(accountNum)) {
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+    
+//    계좌개설
+    public static void createAccount(){
+        System.out.print("계좌번호 : ");
+        String accountNum = sc.nextLine();
+
+        System.out.print("이름 : ");
+        String name = sc.nextLine();
+
+        System.out.print("금액 : ");
+        int balance = sc.nextInt();
+        sc.nextLine();
+
+        accounts.add(new Account(accountNum, name, balance));
+
+        System.out.println("계좌가 개설되었습니다.");
+    }
+
+    public static void cheakBalance() {
+        System.out.print("계좌번호를 입력하세요 : ");
+        String accountNum = sc.nextLine();
+
+        int AN = makeAccouontNum(accountNum);
+        if(AN != -1) {
+            System.out.println("계좌번호 : " + accountNum);
+            System.out.println("잔액 : " + accounts.get(AN).getBalance());
+        } else {
+            System.out.println("계좌번호를 확인해 주세요");
+        }
+    }
+
+    public static void cheakIn(){
+
+        System.out.print("계좌번호 : ");
+        String accountNum = sc.nextLine();
+
+        System.out.print("입금액 : ");
+        int amount = sc.nextInt();
+    }
+
+}
