46c109786311cb467a0ea8dab99aa135ccbb68e8
GyeongHo <kungho84@gmail.com>
2024-04-11 22:47:12 +0900

    아이고 두야///

---
+++ MiniPrj/src/Health2/Health/Res.java
@@ -0,0 +1,39 @@
+package Health2.Health;
+
+public class Res {
+    private String MemberId;  // 회원 아이디
+    private int MemberPw;
+    private String MemberName;  // 회원이름
+    private int Count; // 남은
+
+    public String getMemberId(){
+        return MemberId;
+    }
+    public int getMemberPw(){
+        return MemberPw;
+    }
+    public String getMemberName(){
+        return MemberName;
+    }
+    public void setMemberId(String MemberId){
+        this.MemberId = MemberId;
+    }
+    public void  setMemberPw(int MemberPw){
+        this.MemberPw = MemberPw;
+    }
+    public void setMemberName(String MemberName){
+        this.MemberName = MemberName;
+    }
+    public int getCount(){
+        return Count;
+    }
+    public void setCount(int Count){
+        this.Count = Count;
+    }
+    public void addCount(int input){
+        this.Count += input;
+    }
+    public void subCount(int input){
+        this.Count -= input;
+    }
+}
---
+++ MiniPrj/src/Health2/Health/ResMain.java
@@ -0,0 +1,8 @@
+package Health2.Health;
+
+public class ResMain {
+    public static void main(String[] args) {
+        ResChoice res = new ResChoice();
+        res.resChoice();
+    }
+}
