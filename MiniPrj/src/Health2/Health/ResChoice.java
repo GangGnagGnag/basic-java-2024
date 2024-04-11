9e94545430a07aba402c0617b3bf0d2d5e720990
GyeongHo <kungho84@gmail.com>
2024-04-11 22:54:20 +0900

    일단 여기까지

--- MiniPrj/src/Health2/Health/ResChoice.java
+++ MiniPrj/src/Health2/Health/ResChoice.java
@@ -12,7 +12,7 @@ public class ResChoice {
         int choice;
 
         while (yes) {
-            System.out.println("==========================================================");
+            System.out.println("\n==========================================================");
             System.out.println("1. 회원 등록 | 2. 회원 조회 | 3. 연장 | 4. 회원 삭제 | 5. 종료 ");
             System.out.println("==========================================================");
 
@@ -55,8 +55,7 @@ public class ResChoice {
         Res newMember = new Res();
 
         while (true) {
-            System.out.println();
-            System.out.println("등록하실 ID를 생성하세요.");
+            System.out.println("\n등록하실 ID를 생성하세요.");
             System.out.print(">> ");
             String memberId = sc.nextLine();
             newMember.setMemberId(memberId);
@@ -70,21 +69,17 @@ public class ResChoice {
                     sc.nextLine();
                     newMember.setMemberPw(memberPw);
                 } else {
-                    System.out.println();
-                    System.out.println("잘못된 입력입니다");
-                    System.out.println();
+                    System.out.println("\n 잘못된 입력입니다 \n");
                     sc.next();
                     continue;
                 }
-                System.out.println();
-                System.out.println("회원님 이름을 적어주세요");
+                System.out.println("\n회원님 이름을 적어주세요");
                 System.out.print(">> ");
                 String name = sc.nextLine();
                 newMember.setMemberName(name);
 
                 while (true) {
-                    System.out.println();
-                    System.out.println("등록하실 PT횟수를 입력하세요");
+                    System.out.println("\n등록하실 PT횟수를 입력하세요");
                     System.out.print(">> ");
 
                     if (sc.hasNextInt()) {
@@ -104,14 +99,13 @@ public class ResChoice {
 
                     res.add(newMember);
 
-                    System.out.println();
-                    System.out.println("================================================");
+                    System.out.println("\n================================================");
                     System.out.println("회원 등록이 완료.");
                     System.out.println(newMember.getMemberName() + " 님의 회원번호는" + newMember.getMemberId());
                     System.out.println("(*비밀번호는 다른 사람에게 노출되지않게 주의하여주세요*)");
                     System.out.println("비밀번호는 " + newMember.getMemberPw() + "입니다 ");
                     System.out.println("남은 PT 횟수는 " + newMember.getCount() + "회 입니다");
-                    System.out.println("================================================");
+                    System.out.println("==============================================");
 
                     // PT 입력이 완료되면 루프 탈출
                     break;
@@ -126,21 +120,23 @@ public class ResChoice {
 
     public void memberList() {
         boolean run = false;
-        System.out.println("회원 ID를 입력하세요");
+        System.out.println("\n회원 ID를 입력하세요");
         System.out.print(">> ");
         String memberId = sc.nextLine();
 
-        System.out.println("비밀번호를 입력하세요");
+        System.out.println("\n비밀번호를 입력하세요");
         System.out.print(">> ");
         int memberPw = sc.nextInt();
         sc.nextLine();
 
         for (Res member : res) {
             if (member.getMemberId().equals(memberId) && member.getMemberPw() == memberPw) {
-                System.out.println("회원 정보를 찾았습니다:");
+                System.out.println("\n=================회원 조회=================");
                 System.out.println("이름: " + member.getMemberName());
                 System.out.println("회원번호: " + member.getMemberId());
                 System.out.println("남은 PT 횟수: " + member.getCount());
+                System.out.println("============================================");
+
                 run = true;
                 break;
             }
